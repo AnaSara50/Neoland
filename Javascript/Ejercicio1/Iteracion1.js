19936e04743d9e999db5c1afd3ecba72804db77d
Ana Lafuente <analafuente@MacBook-Pro-de-Ana.local>
2024-03-19 15:30:22 +0100

    interacion1

---
+++ Javascript/Ejercicio1/Iteracion1.js
@@ -0,0 +1,15 @@
+let myFavoriteHero = "Hulk";
+console.log (myFavoriteHero);
+
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. Comentario= Comman kc  Quitar comentario= Comman uc
+
+
+let x = 50;
+console.log (x);
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+
+
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+let h = 5;
+let y =10;
