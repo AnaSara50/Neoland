b51eb281538591bd81f399fa69ada3562cb22ac9
Ana Lafuente <analafuente@MBP-de-Ana.home>
2024-03-19 21:23:36 +0100

    actualizar repo

--- Javascript/Ejercicio1/Iteracion1.js
+++ Javascript/Ejercicio1/Iteracion1.js
@@ -1,8 +1,9 @@
 let myFavoriteHero = "Hulk";
 console.log (myFavoriteHero);
 
-// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. Comentario= Comman kc  Quitar comentario= Comman uc
 
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. Comentario= Comman kc  Quitar comentario= Comman uc
+// git add .  git commit git push
 
 let x = 50;
 console.log (x);
@@ -13,3 +14,44 @@ console.log (x);
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 let h = 5;
 let y =10;
+console.log (h);
+
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let z = 5 + 10;
+console.log (z);
+
+
+// 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+// const character = {name: 'Jack Sparrow', age: 10};
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+console. log (character);
+
+
+// 1.2 Declara 3 variables con los nombres y valores siguientes 
+	// firstName = Jon'; 
+	// lastName = 'Snow'; 
+	// age = 24; 
+// Muestralos por consola de esta forma: 
+	// 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+   let firstName = "Jon";
+    let lastName = "Snow";
+    let age = 24;
+    let resultado = `Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos`;
+    console. log (resultado);
+
+
+    // .3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+// ambos juguetes.
+// const toy1 = {name: 'Buss myYear', price: 19};
+// const toy2 = {name: 'Rallo mcKing', price: 29};
+
+
+
+// 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+// y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+// basePrice más el valor de la variable globalBasePrice.
+// let globalBasePrice = 10000;
+// const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+// const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
