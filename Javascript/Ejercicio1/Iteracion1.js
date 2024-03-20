03e0c40789f2a2887a9313f6790a6c6dae49eb5f
Ana Lafuente <analafuente@MacBook-Pro-de-Ana.local>
2024-03-20 16:56:12 +0100

    Solucion ejercicios

--- Javascript/Ejercicio1/Iteracion1.js
+++ Javascript/Ejercicio1/Iteracion1.js
@@ -1,15 +1,14 @@
+// EJERCICIOS       
+
+// Variables
+
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. Comentario= Comman kc  Quitar comentario= Comman uc
 let myFavoriteHero = "Hulk";
 console.log (myFavoriteHero);
 
-
-// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella. Comentario= Comman kc  Quitar comentario= Comman uc
-// git add .  git commit git push
-
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 let x = 50;
 console.log (x);
-// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-
-
 
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 let h = 5;
@@ -17,10 +16,11 @@ let y =10;
 console.log (h);
 
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
-
 let z = 5 + 10;
 console.log (z);
 
+
+//Variables avanzadas
 
 // 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 // const character = {name: 'Jack Sparrow', age: 10};
@@ -42,16 +42,70 @@ console. log (character);
     console. log (resultado);
 
 
-    // .3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+// 1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 // ambos juguetes.
 // const toy1 = {name: 'Buss myYear', price: 19};
 // const toy2 = {name: 'Rallo mcKing', price: 29};
-
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+let precio=toy1.price + toy2.price;
+console. log (precio); 
 
 
 // 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 // y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
 // basePrice más el valor de la variable globalBasePrice.
-// let globalBasePrice = 10000;
-// const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-// const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+car1.finalPrice = globalBasePrice + car1.basePrice
+car2.finalPrice = globalBasePrice + car2.basePrice
+console. log (car1.finalPrice, car2.finalPrice); 
+
+
+// OPERADORES
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let a=10;
+let b=5;
+console. log (a * b);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+let c=10;
+c /= 2;
+console. log (c);
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+let d=15;
+let e=9;
+console. log (d % e);
+
+
+//1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+//teniendo dos variables p = 10 y j = 5.
+let p=10;
+let j=5;
+o = p + j;
+console. log (o);
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+let g=10;
+let m=5;
+i = g + m;
+console. log (i);
+
+// Arrays
+
+// 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console. log (avengers[0]);
+
+// 1.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+avengers[0]="Ironman";
+console.log (avengers[]);
