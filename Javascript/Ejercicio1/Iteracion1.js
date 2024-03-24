fe9e3376d8033d5707bf859203ad0a062aaeb8c8
Ana Lafuente <analafuente@MBP-de-Ana.home>
2024-03-24 22:29:38 +0100

    ejercicios

--- Javascript/Ejercicio1/Iteracion1.js
+++ Javascript/Ejercicio1/Iteracion1.js
@@ -108,4 +108,95 @@ console. log (avengers[0]);
 // 1.2 Cambia el primer elemento de avengers a "IRONMAN"
 
 avengers[0]="Ironman";
-console.log (avengers[]);
+console.log (avengers);
+
+
+// 1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+console. log (avengers.length);
+
+
+
+// 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+// Muestra en consola el último personaje del array
+ const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+ rickAndMortyCharacters.push ("Morty", "Summer");
+ console. log (rickAndMortyCharacters[4]);
+
+
+
+
+
+// 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+ const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop ();
+console. log (rickAndMortyCharacters2 [0], rickAndMortyCharacters2 [4]);
+
+// 1.6 Elimina el segundo elemento del array y muestra el array por consola.
+ const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice (1, 1);
+console. log (rickAndMortyCharacters3);
+
+// CONDICIONALES
+
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number2 / number1 ==2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2 ) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3 * 5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3 * 5 == number1 && number1 * 2 == number2) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2 / 2 == number1 || number1 / 5 == number3) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
+// Condicionales
+
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+for (i=0; i<10; i++) {
+console. log (i);
+}
+
+
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+// cuando el resto del numero dividido entre 2 sea 0.
+for (i=0; i<10; i++) {
+    if (i % 2 == 0){
+        console. log (i);
+    }
+    }
+
+
+
+// 1.3 Crea un bucle para conseguir dormir contando ovejas. 
+// Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+// Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+// y cambia el mensaje en la décima vuelta a 'Dormido!'.
+for (i=0; i<10; i++) {
+if (i<9) {
+    console. log ("Intentando dormir");
+ } else { console. log ("Dormido!");}
+} 
