4ab3b53fdca85ad4e889f7888b21e38786806108
Ana Lafuente <analafuente@MBP-de-Ana.home>
2024-04-22 08:25:12 +0200

    hubjuegos

--- hubjuegos/src/pages/Juego/Juego.js
+++ hubjuegos/src/pages/Juego/Juego.js
@@ -6,22 +6,24 @@ import "./juego.css";
 const templateAhorcado = ()=> `   
 
 <body>
-<div>
-   <div class="container>
-        <div id="info">
+<div id="cuerpo">
+<div id="container>
+<div id="info">
+        
     <h1 id="titulo">El juego del ahorcado</h1>
     <p id="hiddenWord"></p><br>
     <p id="texto">Introduce una letra</p>
     <input id="input" type="text"><br>
     <button id="button">Comprobar</button> 
     <button id="retryButton"></button>
-    <div id="ahorcado">
+            <div id="ahorcado">
         </div> 
           <button id="volverAJugar">Volver a jugar</button> 
         </div>
-       
-    </div>
-</div>`
+        </div>
+</div>
+</body>`
+
 
 const listeners = ()=>{ 
 const boton = document.getElementById("button")
@@ -32,6 +34,11 @@ const listener = ()=>{
     const boton = document.getElementById("volverAJugar")
     boton.addEventListener("click")
     }
+
+    const listene = ()=>{ 
+        const boton = document.getElementById("retryButton")
+        boton.addEventListener("click")
+        }
 
 
 export const PrintAhorcado =()=>{
